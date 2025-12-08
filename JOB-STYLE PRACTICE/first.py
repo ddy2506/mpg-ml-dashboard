80c79c5c6eb2a42532295dec8b91d3542f6b9e36
Dandy Di <dandydi341@gmail.com>
2025-12-07 20:43:10 -0500

    Delete JOB-STYLE PRACTICE directory
    Signed-off-by: Dandy Di <dandydi341@gmail.com>

--- JOB-STYLE PRACTICE/first.py
+++
@@ -1,9 +0,0 @@
-income = float(input("Enter today income: "))
-expense = float(input("Enter today expense: "))
-
-profit = income - expense
-
-if profit > 0:
-    print("You made profit:", profit)
-else:
-    print("You had a loss:", profit)
