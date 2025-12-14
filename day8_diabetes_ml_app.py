700125a065cd55f78678c54ea3a32064b2ebd9b4
Dandy Di <dandydi341@gmail.com>
2025-12-14 13:40:21 -0500

    Reorder imports and add SSL context for Streamlit
    Signed-off-by: Dandy Di <dandydi341@gmail.com>

--- day8_diabetes_ml_app.py
+++ day8_diabetes_ml_app.py
@@ -1,6 +1,10 @@
-import pandas as pd
-import numpy as np
+import matplotlib.pyplot as plt
 import streamlit as st
+import numpy as np     
+import pandas as pd
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
 
 st.title("Diabetes Risk Prediction Demo")
 
